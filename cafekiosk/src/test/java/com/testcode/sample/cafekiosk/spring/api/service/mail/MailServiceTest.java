646dbef3223139ff7cbd0aefb3a43d2e8d1fcd39
DISNOTACAY <yeonjh915@gmail.com>
2025-03-27 18:14:40 +0900

    section7: Spy를 활용한 선택적 메서드 stubbing  실습

--- cafekiosk/src/test/java/com/testcode/sample/cafekiosk/spring/api/service/mail/MailServiceTest.java
+++ cafekiosk/src/test/java/com/testcode/sample/cafekiosk/spring/api/service/mail/MailServiceTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.times;
 
 import com.testcode.sample.cafekiosk.spring.api.client.mail.MailSendClient;
@@ -15,12 +17,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class) // @Mock 사용시 필수
 class MailServiceTest {
 
-  @Mock
+  @Spy
   private MailSendClient mailSendClient;
   @Mock
   private MailSendHistoryRepository mailSendHistoryRepository;
@@ -32,16 +35,13 @@ class MailServiceTest {
   @Test
   void sendMail() {
     // given
-    // 어노테이션 없이 직접 작성하는 방법
-//    MailSendClient mailSendClient = Mockito.mock(MailSendClient.class);
-//    MailSendHistoryRepository mailSendHistoryRepository = Mockito.mock(MailSendHistoryRepository.class);
-//    MailService mailService = new MailService(mailSendClient, mailSendHistoryRepository);
+//    @Spy 사용시 when 사용 불가 do 사용
+//    Mockito.when(mailSendClient.sendMail(anyString(), anyString(), anyString(), anyString()))
+//        .thenReturn(true);
 
-    Mockito.when(mailSendClient.sendMail(anyString(), anyString(), anyString(), anyString()))
-        .thenReturn(true);
-//    Mockito.when(mailSendHistoryRepository.save(any(MailSendHistory.class)));
-//    mock  객체에 아무것도 지정안하면 예외가 아닌 기본값을 반환,
-//    따라서 이부분 없어도 repository 는 그냥 null 을 반환하고 지나감
+    doReturn(true)
+        .when(mailSendClient) // 내부 여러 메서드가 있을 때, 하위에 명시된 메서드만 목으로 돌림. 그외는 실제 객체로 동작함 (일부만 stubbing하고 싶을 때 사용)
+        .sendMail(anyString(), anyString(), anyString(), anyString());
 
     // when
     boolean result = mailService.sendMail("","","", "");
